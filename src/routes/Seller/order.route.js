0162103bb05f94efcf5d5c432b296f92ef4f77c8
jahdevelops <elijaheze777@gmail.com>
2024-01-24 10:41:46 -0600

    delete route for single order

--- src/routes/Seller/order.route.js
+++ src/routes/Seller/order.route.js
@@ -2,10 +2,8 @@ const express = require("express");
 const router = express.Router();
 const {
   getAllOrders,
-  getOrder,
 } = require("../../controller/seller/order.controller");
 
 router.route("/").get(getAllOrders);
-router.route("/:id").get(getOrder);
 
 module.exports = router;
