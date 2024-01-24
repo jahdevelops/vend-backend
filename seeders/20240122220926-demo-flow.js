6b73c8fc8731f2d01c35c69ebf4905305decffce
jahdevelops <elijaheze777@gmail.com>
2024-01-24 08:18:37 -0600

    update seeds

--- seeders/20240122220926-demo-flow.js
+++ seeders/20240122220926-demo-flow.js
@@ -4,107 +4,114 @@ const { v4: uuid } = require("uuid");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up(queryInterface) {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync("Password$1", salt);
+    const sellerId = uuid();
+    const buyerId = uuid();
+    const adminId = uuid();
+    const walletId = uuid();
+    const brandId = uuid();
+    const categoryId = uuid();
+    const productId = uuid();
+    const balanceId = uuid();
     // Create a user
-    const sellerAccount = await queryInterface.insert(null, "users", {
-      id: uuid(),
+    await queryInterface.bulkInsert("users", [{
+      id: sellerId,
       first_name: "seller",
       last_name: "seller",
       email: "seller@seller.com",
       password: hash,
       role: "seller",
       isVerified: true,
-    });
-
-    await queryInterface.insert(null, "users", {
-      id: uuid(),
+    },
+    {
+      id: adminId,
       first_name: "admin",
       last_name: "admin",
       email: "admin@admin.com",
       password: hash,
       role: "admin",
       isVerified: true,
-    });
-    await queryInterface.insert(null, "users", {
-      id: uuid(),
+    },
+    {
+      id: buyerId,
       first_name: "leke",
       last_name: "leke",
       email: "lekejosh6wf@gmail.com",
       password: hash,
       role: "buyer",
       isVerified: true,
-    });
+    }
+    ])
 
     // Create a wallet for the user
-    const wallet = await queryInterface.insert(null, "wallets", {
-      userId: sellerAccount.id,
-    });
+    await queryInterface.bulkInsert("wallets", [{
+      id: walletId,
+      userId: sellerId,
+    }]);
 
-    const balance = await queryInterface.insert(null, "balances", {
-      walletId: wallet.id,
-      userId: sellerAccount.id,
+    await queryInterface.bulkInsert("balances", [{
+      id: balanceId,
+      walletId,
+      userId: sellerId,
       balance: 0,
+    }]);
+
+    await queryInterface.bulkUpdate('wallets', {
+      accountBalance: balanceId
+    }, {
+      id: walletId
     });
 
-    await queryInterface.update(
-      "Wallet",
-      "wallets",
-      {
-        accountBalance: balance.id,
-      },
-      { where: { id: wallet.id } },
-    );
-
-    await queryInterface.update(
-      "User",
-      "users",
-      {
-        id_number: "testtte",
-        walletId: wallet.id,
-      },
-      { where: { id: sellerAccount.id } },
-    );
+    await queryInterface.bulkUpdate('users', {
+      id_number: "testtte",
+      walletId,
+    }, {
+      id: sellerId
+    });
 
     // Create Brand and Category
-    const brand = await queryInterface.insert(null, "brands", {
+    await queryInterface.bulkInsert("brands", [{
+      id: brandId,
       name: "Addidas",
       description: "Mostly shoes",
-    });
+    }]);
 
-    const category = await queryInterface.insert(null, "categories", {
+    await queryInterface.bulkInsert("categories", [{
+      id: categoryId,
       name: "shoe",
       description: "Mostly shoes",
-    });
+    }]);
 
     // Create Product
-    await queryInterface.insert(null, "products", {
+    await queryInterface.bulkInsert("products", [{
+      id: productId,
       name: "food",
       price: 1000,
       main_image: "https://google.com",
-      sub_images: [
+      sub_images: JSON.stringify([
         "https://google.com",
         "https://google.com",
         "https://google.com",
         "https://google.com",
         "https://google.com",
-      ],
+      ]),
       description: "Seeder Product",
       product_details: "Seeded Product",
       specifications: "Seeded",
-      userId: sellerAccount.id,
-      brandId: brand.id,
-      categoryId: category.id,
-    });
+      inventory: productId,
+      userId: sellerId,
+      brandId: brandId,
+      categoryId: categoryId,
+    }]);
   },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("users");
+  async down(queryInterface) {
+    await queryInterface.bulkDelete("users", {});
     await queryInterface.bulkDelete("wallets");
     await queryInterface.bulkDelete("balances");
     await queryInterface.bulkDelete("brands");
     await queryInterface.bulkDelete("categories");
-    await queryInterface.bulkDelete("products");
   },
 };
