eb7ca1332a059802960111e58f72046f28b9be25
jahdevelops <elijaheze777@gmail.com>
2024-01-24 10:42:29 -0600

    search and return orders belonging to a seller

--- src/controller/seller/order.controller.js
+++ src/controller/seller/order.controller.js
@@ -1,8 +1,6 @@
 const db = require("../../model");
 const catchAsyncErrors = require("../../middlewares/catchAsyncErrors");
-const ErrorHandler = require("../../utils/errorHandler");
 const Order = db.order;
-const { orderNotFound } = require("../../messages/error.messages");
 
 exports.getAllOrders = catchAsyncErrors(async (req, res) => {
   const { id } = req.user;
@@ -12,20 +10,18 @@ exports.getAllOrders = catchAsyncErrors(async (req, res) => {
   const sort = validSortOrders.includes(sortOrder) ? sortOrder : "asc";
   const offset = (page - 1) * pageSize;
 
-  const orders = await Order.findAndCountAll({
-    offset,
-    limit: pageSize,
-    order: [[sortBy, sort]],
-    where: {
-      carts: {
-        [db.Sequelize.Op.contains]: [
-          {
-            sellerId: id,
-          },
-        ],
-      },
-    },
-  });
+  const orders = await Order.findAndCountAll(
+    { 
+      where: {
+        carts: {
+          sellerId: id
+        }
+      },  
+      offset,
+      limit: pageSize,
+      order: [[sortBy, sort]],
+    }
+  )
 
   const response = {
     success: true,
@@ -36,21 +32,4 @@ exports.getAllOrders = catchAsyncErrors(async (req, res) => {
   };
 
   return res.status(200).json(response);
-});
-
-exports.getOrder = catchAsyncErrors(async (req, res, next) => {
-  const { id } = req.params;
-  const order = await Order.findOne({
-    where: {
-      id: id,
-    },
-  });
-  if (!order) {
-    return next(new ErrorHandler(orderNotFound.message, orderNotFound.code));
-  }
-  return res.status(200).json({
-    success: true,
-    message: "Order retrieved successfully",
-    order: order,
-  });
 });
