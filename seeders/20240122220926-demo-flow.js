1339b40e418ee7cda53c8db5b285cd7bdecbe61d
jahdevelops <elijaheze777@gmail.com>
2024-01-24 10:37:40 -0600

    define unique ids

--- seeders/20240122220926-demo-flow.js
+++ seeders/20240122220926-demo-flow.js
@@ -1,20 +1,23 @@
 "use strict";
 const bcrypt = require("bcryptjs");
+const { Op } = require("sequelize");
 const { v4: uuid } = require("uuid");
+
+const sellerId = "8518ee9c-7316-44d9-b11a-3ee15d01033d";
+const buyerId = "2d2ddbb8-b8fc-4df3-b7d6-cabe796c92ad";
+const adminId = "48ebbe82-a651-4030-9ffa-b8214026df10";
+const walletId = "acde3b8d-19bc-4a2e-ae60-2f438a4f1431";
+const brandId = "94fec972-de64-4a6e-b314-0b6d6239bbb6";
+const categoryId = "9070ba13-d989-415c-89a2-d53f962707dc";
+const productId = "13159492-1f3b-4f61-a987-bd595fa0709f";
+const balanceId = "4f25b9bb-4efa-4947-8bc0-ff3fc953c05a";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync("Password$1", salt);
-    const sellerId = uuid();
-    const buyerId = uuid();
-    const adminId = uuid();
-    const walletId = uuid();
-    const brandId = uuid();
-    const categoryId = uuid();
-    const productId = uuid();
-    const balanceId = uuid();
+    
     // Create a user
     await queryInterface.bulkInsert("users", [{
       id: sellerId,
@@ -108,10 +111,22 @@ module.exports = {
   },
 
   async down(queryInterface) {
-    await queryInterface.bulkDelete("users", {});
-    await queryInterface.bulkDelete("wallets");
-    await queryInterface.bulkDelete("balances");
-    await queryInterface.bulkDelete("brands");
-    await queryInterface.bulkDelete("categories");
+    await queryInterface.bulkDelete("users", {
+      id: {
+        [Op.in]: [sellerId, adminId, buyerId]
+      }
+    });
+    await queryInterface.bulkDelete("wallets", {
+      id: walletId
+    });
+    await queryInterface.bulkDelete("balances", {
+      id: balanceId
+    });
+    await queryInterface.bulkDelete("brands", {
+      id: brandId
+    });
+    await queryInterface.bulkDelete("categories", {
+      id: categoryId
+    });
   },
 };
