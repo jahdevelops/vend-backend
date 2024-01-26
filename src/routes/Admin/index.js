d2aeed54fbb9252f6d32e42c8a8935ec7745b89f
jahdevelops <elijaheze777@gmail.com>
2024-01-26 09:44:32 -0600

    add the order route for admin

--- src/routes/Admin/index.js
+++ src/routes/Admin/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const seller = require("./seller.route");
 const product = require("./product.route");
 const user = require("./user.route");
+const order = require("./order.route");
 
 const router = Router();
 
 router.use("/seller", seller);
 router.use("/product", product);
 router.use("/user", user);
+router.use("/order", order);
 
 module.exports = router;
