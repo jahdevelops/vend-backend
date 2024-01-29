4e910aa793da8898d9bfbc7e594d965e2171b879
jahdevelops <elijaheze777@gmail.com>
2024-01-29 09:04:05 -0600

    add transaction entry route for admin api path

--- src/routes/Admin/index.js
+++ src/routes/Admin/index.js
@@ -3,6 +3,7 @@ const seller = require("./seller.route");
 const product = require("./product.route");
 const user = require("./user.route");
 const order = require("./order.route");
+const transaction = require("./transaction.route");
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/seller", seller);
 router.use("/product", product);
 router.use("/user", user);
 router.use("/order", order);
+router.use("/transaction", transaction);
 
 module.exports = router;
