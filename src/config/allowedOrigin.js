9c3f428a364f94da12e5ac3bfb35cce9e3e437cb
jahdevelops <elijaheze777@gmail.com>
2024-02-06 09:52:14 -0600

    add "5174, 5173" ports to list of allowed origins

--- src/config/allowedOrigin.js
+++ src/config/allowedOrigin.js
@@ -1,3 +1,8 @@
-const allowedOrigin = ["http://localhost:4000", "http://localhost:3000"];
+const allowedOrigin = [
+  "http://localhost:4000",
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "http://localhost:5174",
+];
 
 module.exports = allowedOrigin;
